6f04c8634de7fa7177f05669f2237f4dbe061025
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-21 12:04:04 +0300

    Added property resource

---
+++ api/db/migrate/20230421090202_create_properties.rb
@@ -0,0 +1,13 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.string :property_type
+      t.string :address
+      t.string :description
+      t.string :price
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
