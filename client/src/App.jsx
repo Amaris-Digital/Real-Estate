568c4283265fe46cc72305a06769e6f4734d0302
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-22 15:40:45 +0300

    Added sign up sign in and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import SignUp from "./components/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hello from "./components/Home";
+import Login from "./components/Login";
+import SplashScreen from "./pages/SplashScreen";
+import Property from "./components/Property";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [storedToken, setStoredToken] = useState(localStorage.getItem("token"));
+  const [loggedInUserId, setLoggedInUserId] = useState("");
+  useEffect(() => {
+    console.log(storedToken);
+  }, [storedToken]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/api/v1/profile ", {
+      method: "GET",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoggedInUserRole(data.user.role);
+        setLoggedInUserId(data.user.id);
+      });
+  }, [storedToken]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Router>
+        <Routes>
+          {storedToken ? (
+            <>
+              <Route
+                path="/"
+                element={<Hello setStoredToken={setStoredToken} />}
+              />
+              <Route
+                path="/property/:id"
+                element={<Property loggedInUserId={loggedInUserId} />}
+              />
+            </>
+          ) : (
+            <>
+              <Route
+                path="/"
+                element={<SplashScreen setStoredToken={setStoredToken} />}
+              />
+              <Route
+                path="/signup"
+                element={<SignUp setStoredToken={setStoredToken} />}
+              />
+
+              <Route
+                path="/login"
+                element={<Login setStoredToken={setStoredToken} />}
+              />
+            </>
+          )}
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
